8f05849f9ade72d765d290b99dc85acfa92f5f3b
User <user@NB033814.BRQ.COM>
2024-07-11 17:52:34 -0300

    fix timer that was running twice as fast every time a button was pressed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -16,13 +16,16 @@ class ViewController: UIViewController {
         "Hard" : 720
     ]
     var secondsRemaining = 60
+    var timer = Timer()
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
         let hardness = sender.currentTitle! // Soft, Medium, Hard (Optional)
         
         secondsRemaining = eggTime[hardness]!
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
