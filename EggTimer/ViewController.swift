24429c4bdfb3f4428af4e828b2de40cad9847adc
User <user@NB033814.BRQ.COM>
2024-07-11 17:06:50 -0300

    print the minutes for the egg hardness chosen

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,23 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTime = [
+        "Soft" : 5,
+        "Medium" : 7,
+        "Hard" : 12
+    ]
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("Error")
-        }
+        let hardness = sender.currentTitle! // Soft, Medium, Hard (Optional)
         
+        print(eggTime[hardness]!)
     }
     
 }
